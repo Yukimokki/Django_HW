d3a0f8e7cc48de4009aa5f9ce8e1ddb8f8d32053
Yukimokki <yana.domnina@gmail.com>
2024-09-12 23:14:01 +0100

    Home_work_3 - Draft for tutor to check

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from catalog.views import home, contact
+from catalog.views import home, contact, catalog, product_card
 from catalog.apps import CatalogConfig
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
      path('', home),
-     path('contacts/', contact)
+     path('contacts/', contact),
+     path('catalog/', catalog, name='catalog'),
+     path('product/<int:pk>', product_card, name="product_card"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from catalog.models import Product, Category
 
 
 def home(request):
     products = Product.objects.all()
     context = {"products": products}
-    return render(request, 'catalog/products_list.html', context)
+    return render(request, 'catalog/start_page.html', context)
 
 
 def contact(request):
@@ -16,6 +16,12 @@ def contact(request):
         print(f'You have new message from {name}({email}): {message}')
     return render(request, 'catalog/contact.html')
 
+def catalog(request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, 'catalog/products_list.html', context)
 
 def product_card(request, pk):
-    return render(request, 'catalog/home.html')
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, 'catalog/product_card.html', context)
